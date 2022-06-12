5db3026f4e194df356335486ba77bb4e31894cb8
Kevin Millan <millankevin2@gmail.com>
2022-06-13 00:40:10 +0200

    test

---
+++ Dominio/Modelo/LoginModelo.cs
@@ -0,0 +1,8 @@
+﻿namespace Dominio.Modelo
+{
+    public class LoginModelo
+    {
+        public string Usuario { get; set; }
+        public string Contraseña { get; set; }
+    }
+}
---
+++ Dominio/Modelo/UsuarioModelo.cs
@@ -0,0 +1,13 @@
+﻿namespace Dominio.Modelo
+{
+    public class UsuarioModelo
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Identificacion { get; set; }
+        public string Correo { get; set; }
+        public string Telefono { get; set; }
+        public string Direccion { get; set; }
+        public string FechaNacimiento { get; set; }
+    }
+}

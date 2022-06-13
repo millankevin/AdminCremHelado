6e6b147c7d678e0966721b6e16be9e8a3d5b21c6
Kevin Millan <millankevin2@gmail.com>
2022-06-13 02:00:09 +0200

    commit agregar proyecto excluyendo archivos cache

---
+++ Logica/Utilidades/Constantes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logica.Utilidades
+{
+    public static class Constantes
+    {
+        public const string JWT_CLAIM_ID = "cid";
+        public const string JWT_CLAIM_USUARIO = "cusu";
+    }
+}
---
+++ Transversal/Logger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace Transversal
+{
+    public static class Logger
+    {
+        public static void EscribirLogger(string lines)
+        {
+            string path = @"D:\LOG\AdminCH\";
+            string fileName = string.Format("LOG_APP_ADMIN_CH_{0:yyyyMMdd}.txt", DateTime.Now);
+
+            DirectoryInfo dir = new DirectoryInfo(path);
+            if (!dir.Exists)
+                dir.Create();
+
+            try
+            {
+                System.IO.StreamWriter file = new System.IO.StreamWriter(path + fileName, true);
+                file.WriteLine(DateTime.Now.ToString() + ": " + lines);
+                file.Close();
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+    }
+}
